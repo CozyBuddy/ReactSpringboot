6b9c7c00350655c788a98b540020fb427d827148
malgeum <kwonsunny@naver.com>
2024-09-18 15:48:48 +0900

    9.18

--- backend/mallapi/src/main/java/com/zerock/mallapi/controller/ProductController.java
+++ backend/mallapi/src/main/java/com/zerock/mallapi/controller/ProductController.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.zerock.mallapi.dto.PageRequestDTO;
+import com.zerock.mallapi.dto.PageResponseDTO;
 import com.zerock.mallapi.dto.ProductDTO;
+import com.zerock.mallapi.service.ProductService;
 import com.zerock.mallapi.util.CustomFileUtil;
 
 import lombok.RequiredArgsConstructor;
@@ -12,14 +15,18 @@ import lombok.extern.log4j.Log4j2;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -28,26 +35,99 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Log4j2
 @RequestMapping("/api/products")
 public class ProductController {
-    
+    private final ProductService productService;
     private final CustomFileUtil fileUtil;
 
-    @PostMapping("/")
-    public Map<String,String> register(ProductDTO productDTO) {
-         log.info("register:" + productDTO);
-        List<MultipartFile> files = productDTO.getFiles();
+    // @PostMapping("/")
+    // public Map<String,String> register(ProductDTO productDTO) {
+    //      log.info("register:" + productDTO);
+    //     List<MultipartFile> files = productDTO.getFiles();
 
-        List<String> uploadFileNames = fileUtil.saveFiles(files);
+    //     List<String> uploadFileNames = fileUtil.saveFiles(files);
         
-        productDTO.setUploadFileNames(uploadFileNames);
+    //     productDTO.setUploadFileNames(uploadFileNames);
 
-        log.info(uploadFileNames);
-        return Map.of("Result","Success");
-    }
+    //     log.info(uploadFileNames);
+    //     return Map.of("Result","Success");
+    // }
 
     @GetMapping("/view/{fileName}")
     public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName) {
         return fileUtil.getFile(fileName);
     }
+
+    @GetMapping("/list")
+    public PageResponseDTO<ProductDTO> list(PageRequestDTO pageRequestDTO){
+        log.info("list.............." + pageRequestDTO);
+
+        return productService.getList(pageRequestDTO);
+    }
+
+    @PostMapping("/")
+    public Map<String,Long> register(ProductDTO productDTO) {
+        log.info("register: " +productDTO);
+
+        List<MultipartFile> files = productDTO.getFiles();
+
+        List<String> uploadFileNames = fileUtil.saveFiles(files);
+
+        productDTO.setUploadFileNames(uploadFileNames);
+
+        log.info(uploadFileNames);
+
+        Long pno = productService.register(productDTO);
+        
+        return Map.of("result" , pno);
+    }
+    
+    @GetMapping("/{pno}")
+    public ProductDTO read(@PathVariable(name = "pno") Long pno) {
+        return productService.get(pno);
+    }
+
+    @PutMapping("/{pno}")
+    public Map<String,String> modify(@PathVariable(name= "pno")Long pno, ProductDTO productDTO) {
+        productDTO.setPno(pno);
+
+        ProductDTO oldProductDTO = productService.get(pno);
+
+        List<String> oldFileNames = oldProductDTO.getUploadFileNames();
+
+        List<MultipartFile> files = productDTO.getFiles();
+
+        List<String> currentUploadFileNames = fileUtil.saveFiles(files);
+        
+        List<String> uploadFileNames = productDTO.getUploadFileNames();
+
+        if(currentUploadFileNames != null && currentUploadFileNames.size() > 0) {
+            uploadFileNames.addAll(currentUploadFileNames);
+
+        }
+        productService.modify(productDTO);
+
+        if (oldFileNames != null && oldFileNames.size() >0) {
+           List<String> removeFiles = oldFileNames
+           .stream()
+           .filter(fileName -> uploadFileNames.indexOf(fileName) == -1)
+           .collect(Collectors.toList()) ;
+
+           fileUtil.deleteFiles(removeFiles);
+        }
+        return Map.of("Result" , "Success");
+    }
+
+    @DeleteMapping("/{pno}")
+    public Map<String,String> remove(@PathVariable("pno") Long pno) {
+
+        List<String> oldFileNames = productService.get(pno).getUploadFileNames();
+
+        productService.remove(pno);
+        
+        fileUtil.deleteFiles(oldFileNames);
+
+        return Map.of("Result" , "Success");
+    }
+    
     
     
 }
--- backend/mallapi/src/main/java/com/zerock/mallapi/service/ProductServiceImpl.java
+++ backend/mallapi/src/main/java/com/zerock/mallapi/service/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.zerock.mallapi.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -62,6 +63,101 @@ public class ProductServiceImpl implements ProductService {
        .totalCount(totalCount)
        .pageRequestDTO(pageRequestDTO)
        .build() ;
+    }
+
+    @Override
+    public Long register(ProductDTO productDTO) {
+        Product product = dtoToEntity(productDTO) ;
+
+        Product result = productRepository.save(product);
+
+        return result.getPno();
+
+    }
+
+    private Product dtoToEntity(ProductDTO productDTO){
+        Product product = Product.builder()
+        .pno(productDTO.getPno())
+        .pname(productDTO.getPname())
+        .pdesc(productDTO.getPdesc())
+        .price(productDTO.getPrice())
+        .build();
+
+        List<String> uploadFileNames = productDTO.getUploadFileNames();
+
+        if (uploadFileNames == null) {
+            return product;
+            
+        }
+
+        uploadFileNames.stream().forEach(uploadName -> {
+            product.addImageString(uploadName);
+        });
+
+        return product;
+    }
+
+    @Override
+    public ProductDTO get(Long pno) {
+        Optional<Product> result = productRepository.selectOne(pno);
+
+        Product product = result.orElseThrow() ;
+
+        ProductDTO productDTO = entityToDTO(product);
+
+        return productDTO ;
+    }
+
+    private ProductDTO entityToDTO(Product product){
+        ProductDTO productDTO = ProductDTO.builder()
+        .pno(product.getPno())
+        .pname(product.getPname())
+        .pdesc(product.getPdesc())
+        .price(product.getPrice())
+        .build();
+
+        List<ProductImage> imageList = product.getImageList();
+
+        if (imageList == null || imageList.size() == 0) {
+            return productDTO;
+        }
+
+        List<String> fileNameList = imageList.stream().map(productImage -> 
+            productImage.getFileName()).toList();
+
+            productDTO.setUploadFileNames(fileNameList);
+
+            return productDTO;
+        
+    }
+
+    @Override
+    public void modify(ProductDTO productDTO) {
+        Optional<Product> result = productRepository.findById(productDTO.getPno());
+
+        Product product = result.orElseThrow();
+
+        product.changeName(productDTO.getPname());
+        product.changeDesc(productDTO.getPdesc());
+        product.changePrice(productDTO.getPrice());
+
+        product.clearList();
+
+        List<String> uploadFileNames = productDTO.getUploadFileNames() ;
+
+        if(uploadFileNames != null && uploadFileNames.size()>0) {
+            uploadFileNames.stream().forEach(uploadName -> {
+                product.addImageString(uploadName);
+            });
+        }
+        productRepository.save(product);
+
+    }
+
+    @Override
+    public void remove(Long pno) {
+        productRepository.updateToDelete(pno, true);
+        
     }
 
 }
